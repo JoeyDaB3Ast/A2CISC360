be0549e36e07c15414dfd5cff7c97cf88d3de1ca
JoeyDaB3Ast <60456147+JoeyDaB3Ast@users.noreply.github.com>
2025-02-14 23:37:57 -0500

    finished

--- a2-20400130.hs
+++ a2-20400130.hs
@@ -9,7 +9,7 @@ import Data.List
 -- Rename this file to include your student ID: a2-studentid.hs
 -- Also, add your student ID number after the "=":
 student_id :: Integer
-student_id = 
+student_id = 20400130
 
 {- If you are working in a group of 2, uncomment the line below and add the second student's ID number: -}
 -- second_student_id =
@@ -58,9 +58,15 @@ check_that_you_added_your_student_ID_above = ()
   See a2.pdf for instructions.
 -}
 space_by_thousands :: String -> String
-space_by_thousands xs = undefined
-
-
+space_by_thousands inputList =
+  let firstGroupSize = length inputList `mod` 3
+      (firstGroup, leftover) = splitAt (if firstGroupSize == 0 then 3 else firstGroupSize) inputList
+  in firstGroup ++ formatGroups leftover
+  where
+    formatGroups [] = []
+    formatGroups remaining =
+      let (grouping, leftover) = splitAt 3 remaining
+      in " " ++ grouping ++ formatGroups leftover
 {- 
   Q1b. Helper functions:
        You are welcome to use these in your definition of subitize_string,
@@ -100,18 +106,33 @@ isf n
   See a2.pdf for instructions.
 -}
 subitize_string :: (Int -> Int) -> Char -> String -> String
-subitize_string f separator s = undefined
-
-
+subitize_string f delimiter s = insertSpaces s
+  where
+    insertSpaces [] = []
+    insertSpaces remainingDigits = 
+      let (firstGroup, leftover) = first_k_of (f (length remainingDigits)) remainingDigits
+      in firstGroup ++ 
+         if length leftover == 0 
+         then [] 
+         else [delimiter] ++ insertSpaces leftover
 {-
   Q1c. Write the type and definition of the subitize function below.
   See a2.pdf for instructions.
 -}
 -- Don't forget to replace "..." with a type, and uncomment the type declaration on the next line:
 --  subitize :: ...
-subitize     = undefined
---       ^^^ you'll need to add the appropriate number of arguments here
-
+subitize :: (Int -> Int) -> a -> [a] -> [a]
+subitize f delimiter xs = insertDelimiters xs
+  where
+    insertDelimiters [] = []
+    insertDelimiters remaining =
+      let (firstGroup, leftover) = first_k_of (f (length remaining)) remaining
+      in firstGroup ++ 
+         if length leftover == 0 
+         then [] 
+         else 
+           [delimiter] ++ insertDelimiters leftover
+  
 
 {-
   Q1d. Define test cases for your subitize function below.
@@ -120,12 +141,31 @@ subitize     = undefined
   See a2.pdf for instructions.
 -}
 test_subitize1 = subitize tsf ' ' "123456" == "123 456"
-test_subitize2 = undefined
+
+test_subitize2 = subitize isf '.' "100000000" == "10.00.00.000"
+
+test_subitize3 = subitize tsf ' ' "abc" == "abc"
+
+test_subitize4 = subitize tsf 0 [1,1,1,1,1,1] == [1,1,1,0,1,1,1]
+
+test_subitize5 = subitize tsf '*' "abcdefghij" == "a*bcd*efg*hij"
+
+test_subitize6 = subitize tsf 0 [1,1,1,1,1,1,1] == [1,0,1,1,1,0,1,1,1]
+
+test_subitize7 = subitize tsf "+" ["+","1","b","1","1","1","1"]== ["+", "+", "1", "b", "1", "+", "1", "1", "1"]
+
+test_subitize8 = subitize tsf "" []== []
 
 
 test_all_subitize = [
   test_subitize1,
-  test_subitize2
+  test_subitize2,
+  test_subitize3,
+  test_subitize4,
+  test_subitize5,
+  test_subitize6,
+  test_subitize7,
+  test_subitize8
   ]
 
 -- print these to display the results of the tests you have defined
@@ -147,17 +187,23 @@ data Song = Overlay Song Song
           deriving (Show, Eq)    -- writing Eq here lets us use == to compare Songs
           
 {-
-  Q3a. sing: See a2.pdf for complete instructions.
+  Q2a. sing: See a2.pdf for complete instructions.
 
   Hint: You may be able to simplify your code by writing a helper function.
 -}
 
 sing :: Song -> Song
 
--- (replace this line with your clauses)
+sing (Overlay (Overlay (Note x) right) _)
+  | not (null x) && head x == '1' = right
 
-sing other = other      -- (you're allowed to change this line if you need to)
 
+sing (Overlay (Overlay (Overlay (Note y) s1) s2) s3)
+  | not (null y) && head y == 'S' = Overlay (Overlay s1 s3) (Overlay s2 s3)
+
+sing (Overlay left right) = Overlay (sing left) (sing right)
+
+sing other = other
 
 
 ascend = Overlay (Overlay (Note "S0") (Note "11")) (Note "12")
@@ -184,7 +230,12 @@ test_sing = test_sing1 && test_sing2 && test_sing3 && test_sing4
 -}
 
 repeat_sing :: Song -> Song
-repeat_sing s = undefined
+repeat_sing song =
+  let simplifiedSong = sing song
+  in if simplifiedSong == song 
+     then song
+     else
+       repeat_sing simplifiedSong
 
 test_repeat1 = repeat_sing (Overlay ascend (Note "Z")) == Note "Z"
 
